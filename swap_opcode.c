9b64fd706663d1efe85071b0a519e7e1515e8ec6
Ronald <nottinsonstephen@gmail.com>
2023-05-23 13:34:50 -0700

    monty swap opcode

---
+++ swap_opcode.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * m_swap - adds the top two elements of the stack.
+ * @h: stack head
+ * @line_num: line_number
+ * Return: null
+*/
+void m_swap(stack_t **h, unsigned int line_num)
+{
+	int count = 0, aux;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
+		fclose(bus.file);
+		free(bus.cont);
+		free_stack(*h);
+		exit(EXIT_FAILURE);
+	}
+
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
+}
