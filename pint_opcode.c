183bac271559a589193f7a0528e574b39122cee6
Ronald <nottinsonstephen@gmail.com>
2023-05-24 06:52:49 -0700

    pint opcode

--- pint_opcode.c
+++ pint_opcode.c
@@ -10,9 +10,6 @@ void m_pint(stack_t **h, unsigned int line_num)
 	if (*h == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
-		fclose(bus.file);
-		free(bus.cont);
-		free_stack(*h);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*h)->n);
