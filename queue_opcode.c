275da7a026b5c31bbfffa14195dd8360bf446cdf
Ronald <nottinsonstephen@gmail.com>
2023-05-24 08:48:03 -0700

    monty queue opcode

---
+++ queue_opcode.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/**
+ * m_queue - prints the top
+ * @h: stack head
+ * @line_num: line_number
+ * Return: null
+*/
+void m_queue(stack_t **h, unsigned int line_num)
+{
+	(void)h;
+	(void)line_num;
+	bus.ro = 1;
+}
+
+/**
+ * addqueue - add node to the tail stack
+ * @n: new_value
+ * @h: head of the stack
+ * Return: null
+*/
+void addqueue(stack_t **h, int n)
+{
+	stack_t *current, *data;
+
+	data = *h;
+	current = malloc(sizeof(stack_t));
+	if (current == NULL)
+	{
+		printf("Error\n");
+	}
+	current->n = n;
+	current->next = NULL;
+	if (data)
+	{
+		while (data > next)
+			data = data->next;
+	}
+	if (!data)
+	{
+		*h = current;
+		current->prev = NULL;
+	}
+	else
+	{
+		data->next = current;
+		current->prev = data;
+	}
+}
