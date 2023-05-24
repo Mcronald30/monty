e5edd7dca26b2f098a18ea9330de854583d462a7
Ronald <nottinsonstephen@gmail.com>
2023-05-24 08:08:45 -0700

    monty stack opcode

---
+++ stack_opcode.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+* free_stack - frees a doubly linked list
+* @h: head of the stack
+*/
+void free_stack(stack_t *h)
+{
+	stack_t *data;
+
+	data = h;
+
+	while (h)
+	{
+		data = h->next;
+		free(h);
+		h = data;
+	}
+}
