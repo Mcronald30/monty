ff1f988f571ad728a11da5d1ae15656ce281bc45
Ronald <nottinsonstephen@gmail.com>
2023-05-26 08:05:06 -0700

    monty opcodes 0x19. C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -37,9 +37,9 @@ extern bus_t bus;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -52,8 +52,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
